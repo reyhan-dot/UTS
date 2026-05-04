752c116dfd4e97cc07d0bac344a38e40a40ba1cf
reyhan-dot <reyhanalakmal@gmail.com>
2026-05-04 19:27:32 +0700

    Refactor menu options and improve code structure

--- UTS_PBO_kelompok_3.java
+++ UTS_PBO_kelompok_3.java
@@ -17,8 +17,10 @@ public class UTS_PBO_kelompok_3 {
 
     public static void main(String[] args) {
      Scanner scanner = new Scanner(System.in);
+     
         Mahasiswa[] daftarMhs = new Mahasiswa[10];
         int jumlah = 0;
+        
         while(true){
             System.out.println("\n=== MENU MENEJEMEN MAHASISWA ===\n");
             System.out.println("1. Tambah Mahasiswa");
@@ -31,12 +33,14 @@ public class UTS_PBO_kelompok_3 {
             System.out.print("Pilih Menu: ");
             int pilihan = scanner.nextInt();
              scanner.nextLine();
+             
+            switch (pilihan){
                 
-            if ( pilihan == 1){
-                if (jumlah < 10){
-                System.out.println("\n=== Tambah Mahasiswa ===");
-                
-                System.out.print("Masukkan Nama: ");
+                case 1 :
+                System.out.println("Tambahkan Mahasiswa:");
+                        
+                if (jumlah <10){
+                 System.out.print("Masukkan Nama: ");
                 String name = scanner.nextLine();
                 
                 
@@ -52,83 +56,74 @@ public class UTS_PBO_kelompok_3 {
                 
                 System.out.print("Masukkan IPK: ");
                 Double ipk = scanner.nextDouble();
-                scanner.nextLine();
+                
             
                 Mahasiswa mhs = new Mahasiswa(nim, name, jurusan, semester, ipk);
                     daftarMhs[jumlah] = mhs;
                     jumlah++;
+                    
                 }else {
                     System.out.println("Data Penuh");
                 }
-            }
-            if (pilihan == 2){
-            
-            for (int i = 0; i < jumlah; i++) {
-                    daftarMhs[i].tampilkanData();
-            }
-            if (pilihan == 3){
-            System.out.println("Cari Berdasarkan NIM: ");
-            int cariNim = scanner.nextInt();
-            scanner.nextLine();
-
-             boolean ditemukan = false;
-
-            for (int i = 0; i < jumlah; i++) {
-            if (daftarMhs[i].getNim() == cariNim) {
-            System.out.println("Data ditemukan:");
-            daftarMhs[i].tampilkanData();
-            daftarMhs[i].getNim();
-            ditemukan = true;
-            break;
-            }
-            }
-
-             if (!ditemukan) {
-                System.out.println("Data tidak ditemukan!");
-            }
-            }
-            if (pilihan == 4){
-           System.out.println("Mahasiswa yang Lulus: ");
-           boolean ada = false;
-
-            for (int i = 0; i < jumlah; i++) {
-                 if (daftarMhs[i].isLulus()) {
-            daftarMhs[i].tampilkanData();
-            daftarMhs[i].getNim();
-            ada = true;
-             }
-             }
-
-            if (!ada) {
-            System.out.println("Tidak ada mahasiswa yang lulus.");
-             }
-            }
-            if (pilihan == 5){
-           System.out.println("Semester Dinaikkan");
                 
-            for (int i = 0; i < jumlah; i++) {
-                    daftarMhs[i].naikSemester();
+                case 2 :
+                    for (int i = 0; i < jumlah; i++) {
+                    daftarMhs[i].tampilkanData();
                 }
+                    
+                case 3:
+                System.out.println("Cari Berdasarkan NIM: ");
+                int cariNim = scanner.nextInt();
+                scanner.nextLine();
+
+                boolean ditemukan = false;
+
+                for (int i = 0; i < jumlah; i++) {
+                    if (daftarMhs[i].getNim() == cariNim) {
+                    System.out.println("Data ditemukan:");
+                    daftarMhs[i].tampilkanData();
+                    daftarMhs[i].getNim();
+                    ditemukan = true;
+                    break;
+                    }
+                }
+
+                if (!ditemukan) {
+                System.out.println("Data tidak ditemukan!");
+                }
+                
+                case 4:
+                System.out.println("Mahasiswa yang Lulus: ");
+                boolean ada = false;
+
+                for (int i = 0; i < jumlah; i++) {
+                    if (daftarMhs[i].isLulus()) {
+                    daftarMhs[i].tampilkanData();
+                    daftarMhs[i].getNim();
+                    ada = true;
+                    }
+                }
+
+                if (!ada) {
+                System.out.println("Tidak ada mahasiswa yang lulus.");
+                }  
+                
+                case 5:    
+                System.out.println("Semester Dinaikkan");
+                
+                for (int i = 0; i < jumlah; i++) {
+                daftarMhs[i].naikSemester();
+                }
+                
+                case 6:
+                System.out.println("Program Berakhir");
+                break; 
+
             }
-            if (pilihan == 6){
-           System.out.println("Program Berakhir");
-           
-           
-            }else {
-            System.out.println("invalid");
-            }
-           
+            scanner.close();
         }
-      scanner.close();
-    }
-}
-}
-        
-       
-       
-
-        
+                
+            
+    }  
     
-    
-
-
+}
