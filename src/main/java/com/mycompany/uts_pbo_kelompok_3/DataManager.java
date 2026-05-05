e58c2f23c4f32ba957d940db6bbb22edd2cd4947
reyhan-dot <reyhanalakmal@gmail.com>
2026-05-05 13:00:21 +0700

    Create DataManager.java for file-based data persistence of mahasiswa records

---
+++ src/main/java/com/mycompany/uts_pbo_kelompok_3/DataManager.java
@@ -0,0 +1,11 @@
+public class DataManager {
+    // Method to save data to file
+    public void simpanData() {
+        // Implementation logic to save mahasiswa records to a file
+    }
+
+    // Method to load data from file
+    public void muatData() {
+        // Implementation logic to load mahasiswa records from a file
+    }
+}
